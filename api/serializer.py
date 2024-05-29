46e7543a4f155815a23a80a2e1f8a9296e90b0c0
inverse <rajnepale25@gmail.com>
2024-05-29 22:59:33 +0530

    setting up serializer , mongoconnection ,views, urls except urls of app

---
+++ api/serializer.py
@@ -0,0 +1,14 @@
+# serializers.py
+
+from rest_framework import serializers
+from datetime import datetime
+
+class UserModelSerializer(serializers.Serializer):
+    name = serializers.CharField(max_length=255)
+    email = serializers.EmailField()
+    created_at = serializers.DateTimeField(default=datetime.now)
+    roll = serializers.CharField(max_length=50)
+    img = serializers.CharField(required=False, allow_blank=True)
+
+    def create(self, validated_data):
+        return validated_data
--- api/views.py
+++ api/views.py
@@ -1,3 +1,70 @@
 from django.shortcuts import render
+from rest_framework import viewsets, status
+from rest_framework.response import Response
+from rest_framework.mixins import ListModelMixin, CreateModelMixin
+from rest_framework.views import APIView
+from .connection import user_collection ,db ,fs
+from .serializer import *
+from datetime import datetime
+from bson import ObjectId
 
-# Create your views here.
+
+class UsersViewset(ListModelMixin, CreateModelMixin, viewsets.GenericViewSet):
+    serializer_class = UserModelSerializer
+
+    def get_queryset(self):
+        documents = list(user_collection.find({}, {"_id": 0}))
+        return documents
+
+    def list(self, request, *args, **kwargs):
+        queryset = self.get_queryset()
+        print("data is going")
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def create(self, request, *args, **kwargs):
+        data = request.data
+        print(data, "sdffff")
+
+     
+        image = request.FILES.get('img')
+        if image:
+            image_id = self.handle_image_upload(image)
+            image_id_str = str(image_id) 
+        else:
+            image_id_str = None
+
+        
+        user = {
+            "name": data.get("name"),
+            "email": data.get("email"),
+            "created_at": datetime.now(),
+            "roll": data.get("roll"),
+            "img": image_id_str 
+        }
+
+        user_collection.insert_one(user)
+
+        return Response(status=status.HTTP_201_CREATED)
+
+    def handle_image_upload(self, image):
+       
+        image_id = fs.put(image, filename=image.name, content_type=image.content_type)
+        return image_id
+
+
+
+
+
+
+class ServeImageView(APIView):
+    def get(self, request, image_id):
+        try:
+            file_id = ObjectId(image_id)
+            grid_out = fs.get(file_id)
+            # Set the Content-Type header to indicate that the response contains image data
+            response = Response(grid_out.read(), content_type=grid_out.content_type)
+            return response
+        except Exception as e:
+            print(e)
+            raise Response("Image not found")
