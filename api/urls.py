36b93964c03a414818d68cc75ec21eeca38d348b
inverse <rajnepale25@gmail.com>
2024-05-30 23:18:08 +0530

    changes in urls pattern and install cors headers to allow frontend

--- api/urls.py
+++ api/urls.py
@@ -5,7 +5,7 @@ from .views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("",UsersViewset.as_view({'get': 'list','post':'create'}),name="user") ,
-    path("<str:image_id>",ServeImageView.as_view(),name="img")
+    path("/users",UsersViewset.as_view({'get': 'list','post':'create'}),name="users") ,
+    path("/images/<str:image_id>/",ServeImageView.as_view(),name="img")
    
 ]
